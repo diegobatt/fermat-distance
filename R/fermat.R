e42a8ba68127678a2f2c4152167d6bae94b521dc
diegobatt <diegobatt@diegobatt.com>
2023-05-29 15:01:50 -0300

    Fixed names and added tests

--- R/fermat.R
+++ R/fermat.R
@@ -3,14 +3,8 @@ fermatDistance <- setClass(
     "fermatDistance", slots = c(sp = "matrix", method = "character")
 )
 
-
 #' @export
-distances <- function(object) {
-    UseMethod("distances")
-}
-
-#' @export
-distances.fermatDistance <- function(object) {
+as.matrix.fermatDistance <- function(object) {
     object@sp
 }
 
@@ -32,20 +26,21 @@ get_fermat_distance <- function(X, method = "full", alpha = 2, landmarks_frac = 
     n <- nrow(X)
     dist_matrix <- as.matrix(dist(X, diag = FALSE, upper = FALSE)) ^ alpha
     if (method == "knn") {
-        k <- as.integer(sqrt(ncol(dist_matrix)))
-        for (i in seq_len(ncol(dist_matrix))) {
+        k <- as.integer(sqrt(n))
+        for (i in 1:n) {
             col_dists <- dist_matrix[, i]
             min_k_dist <- sort(col_dists)[k + 1]
-            remove_nodes <- dist_matrix[, i] > min_k_dist
-            # 0 in the edge removes the node
-            dist_matrix[remove_nodes, i] = 0
+            remove_nodes <- dist_matrix[, i] >= min_k_dist
+            # # 0 in the edge removes the node
+            dist_matrix[remove_nodes, i] <- 0
         }
     }
-    g <- igraph::graph_from_adjacency_matrix(dist_matrix, weighted = TRUE, mode="undirected")
+    g <- igraph::graph_from_adjacency_matrix(dist_matrix, weighted = TRUE)
     if (method == "landmarks") {
         landmarks_ids <- sample(n, as.integer(n * landmarks_frac))
         landmarks_sp <- igraph::shortest.paths(g, v = landmarks_ids)
         sp <- matrix(0, n, n)
+        dimnames(sp) <- dimnames(dist_matrix)
         for (i in 1:n) {
             sp[i, ] <- matrixStats::colMins(landmarks_sp[, i] + landmarks_sp)
         }
